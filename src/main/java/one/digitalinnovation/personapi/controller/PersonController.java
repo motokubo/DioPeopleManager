28ff434f8bd4c197f7b5cff6d7dd19acdb6a4d53
motokubo <jorgehideki10@hotmail.com>
2021-08-04 14:17:08 -0300

    FirstCommit

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation.personapi.controller;
+
+import one.digitalinnovation.personapi.entity.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public String getBook(){
+        Person person = new Person();
+        person.getId();
+        return "API Test!";
+    }
+}
